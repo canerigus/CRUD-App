1c9c2db8c100cf88534c7a6071eb5e8c2aa43c84
canerigus <coderigus@gmail.com>
2022-01-30 21:56:05 +0300

    crud added

--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -5,6 +5,10 @@ import { getRepository } from 'typeorm';
 import { User } from '../entity/User';
 import { validate } from 'class-validator';
 
+/* export const uploadProfilePic: RequestHandler = (req, res, next) => {
+	upload.single('image')
+} */
+
 //generating JWT token function.
 export const generateAccessToken = (user: Object) => {
 	return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET!, { expiresIn: '15m' });
@@ -47,70 +51,18 @@ export const validateUser: RequestHandler = async (req, res, next) => {
 	const loggedUsername = '' + req.headers['username'];
 	const loggedUser = await getRepository(User).findOne({ username: loggedUsername });
 	try {
-		const user = await getRepository(User).findOne(req.params.id);
+		const userid = req.params.id
+		const user = await getRepository(User).findOne(userid);
 		if (user.id === loggedUser.id) {
 			next();
 		} else {
-			req.flash('error', 'User not validated!!');
+			req.flash('error', 'User not validated!! aaaaaaaaaa');
 			res.status(401).redirect(`/login`);
 		}
 	} catch (error) {
-		console.log('User Not Validate ' + error);
+		console.log('User Not Validate dsfsdfdsfdsfds' + error);
 		res.status(401).redirect(`/login`);
 	}
 };
 
-export const updateUserInfo: RequestHandler = async (req, res, next) => {
-	const id = req.params.id;
-	const { name, surname, email, occupation, city } = req.body;
-	const username: string = req.body.username;
-	const user = await getRepository(User).findOne(id);
-	if (user) {
-		const errors = await validate({ name, surname, username, email, occupation, city });
-		if (errors.length > 0) {
-			throw new ExpressError(`Validation failed!`, 401);
-		} else {
-			user.name = name;
-			user.surname = surname;
-			user.username = username;
-			user.email = email;
-			user.occupation = occupation;
-			user.city = city;
-			await getRepository(User)
-				.createQueryBuilder()
-				.update(User)
-				.set({
-					name: name,
-					surname: surname,
-					username: username,
-					email: email,
-					occupation: occupation,
-					city: city
-				})
-				.where('id = :id', { id: id })
-				.execute();
-			const updatedUser = await getRepository(User).findOne(id);
-			req.headers['username'] = updatedUser.username;
-			next();
-		}
-	} else {
-		console.log('user ve id yok !');
-		res.status(401).redirect(`/profile/${id}`);
-	}
-};
 
-export const deleteUser: RequestHandler = async (req, res, next) => {
-	const loggedUsername = '' + req.headers['username'];
-	const loggedUser = await getRepository(User).findOne({ username: loggedUsername });
-	if (loggedUser) {
-		const id = loggedUser.id;
-		try {
-			await getRepository(User).createQueryBuilder().delete().from(User).where('id = :id', { id: id }).execute();
-			next();
-		} catch (error) {
-			console.log('delete Failed!');
-			console.log(error);
-			res.status(401).render(`home`);
-		}
-	}
-};
