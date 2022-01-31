d1a1edc8a3063a898cc424dd744da47514045173
canerigus <coderigus@gmail.com>
2022-01-31 21:57:20 +0300

    ormconfigupdate

---
+++ ormconfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+   type: "mysql",
+   host: "localhost",
+   port: process.env.DB_PORT,
+   username: process.env.DB_USERNAME,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_NAME,
+   synchronize: true,
+   logging: false,
+   entities: [
+      "src/entity/**/*.ts"
+   ]
+}
